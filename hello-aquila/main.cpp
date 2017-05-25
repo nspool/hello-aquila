40a8c1c9c88044336fa11aedc76ada47cdf7a748
nspool <nspool@gmx.com>
2017-05-25 20:29:52 +1000

    Sawtooth generator example

--- hello-aquila/main.cpp
+++ hello-aquila/main.cpp
@@ -11,12 +11,14 @@
 #include "aquila/global.h"
 #include "aquila/source/WaveFileHandler.h"
 #include "aquila/source/generator/WhiteNoiseGenerator.h"
+#include "aquila/source/generator/TriangleGenerator.h"
+
 
 int main(int argc, const char * argv[]) {
 
     // Write some white noise out to a file to make sure it works..
     
-    Aquila::WaveFileHandler filehandle("whitenoise.wav");
+    Aquila::WaveFileHandler whitenoise("whitenoise.wav");
     
     Aquila::WhiteNoiseGenerator whiteNoiseGenerator(10000);
     
@@ -24,7 +26,19 @@ int main(int argc, const char * argv[]) {
     
     whiteNoiseGenerator.generate(100000);
     
-    filehandle.save(whiteNoiseGenerator);
+    whitenoise.save(whiteNoiseGenerator);
+    
+    // Use the sawtooth generator to generate the sound of a plane from an old flight sim
+    
+    Aquila::WaveFileHandler sawtooth("sawtooth.wav");
+    
+    Aquila::TriangleGenerator triangleGenerator1(10000);
+    triangleGenerator1.setAmplitude(8192).setFrequency(100).setAmplitude(8192).generate(10000);
+    
+    Aquila::TriangleGenerator triangleGenerator2(10000);
+    triangleGenerator2.setAmplitude(8192).setFrequency(50).setAmplitude(8192).generate(10000);
+    
+    sawtooth.save(triangleGenerator1 + triangleGenerator2);
     
     return 0;
 }
