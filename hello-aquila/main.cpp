2741484b48ca760bc5a0df0f8f3e014b7cf98e81
nspool <nspool@gmx.com>
2017-11-15 21:11:08 +1100

    WIP: Play generated audio using SDL2

--- hello-aquila/main.cpp
+++ hello-aquila/main.cpp
@@ -5,8 +5,10 @@
 //  Created by nsp on 24/5/17.
 //  Copyright © 2017 nspool. All rights reserved.
 //
-
 #include <iostream>
+
+#include <SDL2/SDL.h>
+#include <SDL2_mixer/SDL_mixer.h>
 
 #include "aquila/global.h"
 #include "aquila/source/WaveFileHandler.h"
@@ -27,6 +29,27 @@ int main(int argc, const char * argv[]) {
     
     whitenoise.save(whiteNoiseGenerator);
     
+    // Attempt to play the audio directly from SDL
+    
+    
+    // Initialize SDL
+    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
+        return -1;
+    }
+    
+    //Initialize SDL_mixer
+    if( Mix_OpenAudio(22050, MIX_DEFAULT_FORMAT, 2, 4096) == -1 ) {
+        return -1;
+    }
+
+    // TODO: Convert into a buffer that SDL can use
+    
+//    auto whitenoiseWav = Mix_QuickLoad_WAV(whitenoise);
+//
+//    Mix_PlayChannel(-1, whitenoiseWav, 0);
+
+    Mix_CloseAudio();
+
     // Use the sawtooth generator to generate the sound of a plane from an old flight sim
     
     Aquila::WaveFileHandler sawtooth("sawtooth.wav");
